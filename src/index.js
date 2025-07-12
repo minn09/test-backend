14776c08a9c0ccee4d4df026596ca39487ced131
Jonathan <jonathan.huertas.contreras@gmail.com>
2025-07-12 15:38:30 -0500

    commit5

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 // Ruta GET simple
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo! 🌊 API de Surf para Desarrolladores');
+  res.send('¡Hola, mundo! 🌊 API de Surf para Desarrolladores!');
 });
 
 // Ruta POST de ejemplo
